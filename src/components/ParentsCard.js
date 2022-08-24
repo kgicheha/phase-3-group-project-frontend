0c32cf68413dc39611510708a22481f975773898
Kevin Gicheha <gichehakevin@gmail.com>
2022-08-23 18:36:22 -0700

    testing

--- src/components/ParentsCard.js
+++ src/components/ParentsCard.js
@@ -3,7 +3,8 @@ import React from 'react'
 const ParentsCard = ({image, numOfPets, serviceYears}) => {
   return (
     <div>
-        <img src={image}/>
+      <p>Tesing</p>
+        <img src={image} alt="Parent"/>
         <h3>Number of Pets: {numOfPets}</h3>
         <h3>Years of Service: {serviceYears}</h3>
     </div>
