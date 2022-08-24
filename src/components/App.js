3c798677646bea8bf66abd65ed32cc1dc2f7e678
+ <+dannaewaltz@gmail.com>
2022-08-24 15:51:38 -0600

    pets

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import NavBar from './NavBar'
 import ParentsContainer from './ParentsContainer'
+import PetsContainer from './PetsContainer'
 // import { Route, Switch } from 'react-router-dom';
 
+var test = 1;
 
 const App = () => {
   return (
     <div>
       <NavBar />
-      <ParentsContainer />
-    </div>
+      {/* <ParentsContainer /> */}
+      <PetsContainer s_p ={"parents"} id = {test}/>
+     </div>
   )
 }
 
