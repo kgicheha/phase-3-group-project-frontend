b36763ec6f1e7e64fce8ea93cf40497516d7a90b
Karter <105955246+kdliving14@users.noreply.github.com>
2022-08-25 21:26:33 -0700

    Update Shelters.js
    update for navigation

--- src/components/Shelters.js
+++ src/components/Shelters.js
@@ -2,16 +2,24 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 
-const Shelters = ({name, image, location, hours, no_kill, id}) => {
+const Shelters = ({name, image, location, hours, no_kill, id, setS_P, setID}) => {
+
+  function handleClick()
+  {
+    setID(id);
+    setS_P("shelters");
+  }
+
   return (
-    <div>
+    
+    <div className='Shelter'>
         <img src={image} alt={"Shelter"}/>
         <h3>Name: {name}</h3>
         <h3>Location: {location}</h3>
         <h3>Hours: {hours}</h3>
         <h3>Type: {no_kill ? "No-Kill Shelter" : "Traditional Shelter"}</h3>
-        <button><NavLink to={`/shelters/${id}/pets`}exact>Pets</NavLink></button>
-        <button><NavLink to={`/shelters/${id}/parents`}exact>Parents</NavLink></button>
+        <button onClick ={handleClick}><NavLink to={`/shelters/${id}/pets`}>Pets</NavLink></button>
+        <button onClick ={handleClick}><NavLink to={`/shelters/${id}/parents`}>Parents</NavLink></button>
     </div>
   )
 }
