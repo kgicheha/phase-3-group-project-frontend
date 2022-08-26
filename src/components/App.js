bed454087af861981a092fd20e77347421fb2515
Karter <105955246+kdliving14@users.noreply.github.com>
2022-08-25 21:08:09 -0700

    Update App for Routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,46 @@
-import React from 'react'
-import { Route, Routes} from "react-router-dom";
+import {React, useState} from 'react';
+import {Route, Routes} from "react-router-dom";
 
-import NavBar from './NavBar'
-import About from './About'
-import ParentsAll from './ParentsAll'
-import SheltersContainer from './SheltersContainer'
-import PetsAll from './PetsAll'
-import Pet from './Pet_Instance'
+import NavBar from './NavBar';
+import About from './About';
+import ParentsAll from './ParentsAll';
+import SheltersContainer from './SheltersContainer';
+import PetsAll from './PetsAll';
 import PetsContainer from './PetsContainer';
 import ParentsContainer from './ParentsContainer';
-
-var id = 1;
-var s_p = "shelters";
+import PetInstance from './PetInstance';
 
 const App = () => {
+
+  const [s_p, setS_P] = useState("");
+  const [iD, setID] = useState(1);
+
   return (
     <div>
       <NavBar />
       <Routes>
         <Route path="/" element={<About/>}></Route>
-        <Route path="/shelters" element={<SheltersContainer/>}></Route>
-        <Route path="/parents" element={<ParentsAll/>}></Route>
-        <Route path="/pets" element={<PetsAll />}></Route>
-        <Route path={`/${s_p}/${id}/pets`} element={<PetsContainer s_p={s_p} id={id}/>}></Route>
-        <Route path={`/pet/${id}`} element= {<Pet id={id}/>}></Route>
-        <Route path={`/shelters/${id}/parents`} element={<ParentsContainer id={id}/>}></Route>
+
+        <Route path={`/parents/1/pets`} element={<PetsContainer s_p={"parents"} iD={iD} setID = {setID}/>}></Route>
+
+        <Route path="/shelters" element={<SheltersContainer setS_P ={setS_P} setID={setID}/>}></Route>
+
+        <Route path="/parents" element={<ParentsAll setS_P ={setS_P} setID={setID}/>}></Route>
+        <Route path="/pets" element={<PetsAll setID={setID}/>}></Route>
+        <Route path={`/shelters/${1}/pets`} element={<PetsContainer s_p={"shelters"} iD={iD} setID = {setID}/>}></Route>
+        <Route path={`/shelters/${2}/pets`} element={<PetsContainer s_p={"shelters"} iD={iD} setID = {setID}/>}></Route>
+        <Route path={`/shelters/${3}/pets`} element={<PetsContainer s_p={"shelters"} iD={iD} setID = {setID}/>}></Route>
+
+        <Route path={`/shelters/${1}/parents`} element={<ParentsContainer iD={iD} setID = {setID} setS_P ={setS_P}/>}></Route>
+        <Route path={`/shelters/${2}/parents`} element={<ParentsContainer iD={iD} setID = {setID} setS_P ={setS_P}/>}></Route>
+        <Route path={`/shelters/${3}/parents`} element={<ParentsContainer iD={iD} setID = {setID} setS_P ={setS_P}/>}></Route>
+
+        <Route path={`/parents/${iD}/pets`} element={<PetsContainer s_p={"parents"} iD={iD} setID = {setID}/>}></Route>
+        
+        <Route path={`/pet/${iD}`} element= {<PetInstance id={iD}/>}></Route>
+
+        <Route path={`/parents/1/pets`} element={<PetsContainer s_p={"parents"} iD={iD} setID = {setID}/>}></Route>
+        
       </Routes>
     </div>
   )
