5c7321fc09fd9de614d2054041207862ce5cd885
Karter <105955246+kdliving14@users.noreply.github.com>
2022-08-25 21:21:33 -0700

    Update ParentsCard.js
    updated for navigation

--- src/components/ParentsCard.js
+++ src/components/ParentsCard.js
@@ -1,14 +1,23 @@
-import React from 'react'
+import {React} from 'react'
+import { Link } from 'react-router-dom'
 import { Container, Image } from 'semantic-ui-react'
 
 
-const ParentsCard = ({image, name, numOfPets, serviceYears}) => {
+const ParentsCard = ({name, image, numOfPets, serviceYears, setID, id, setS_P}) => {
+  
+  function handleClick()
+  {
+    setS_P("parents");
+    setID(id);
+  }
+
   return (
-    <Container className ="card">
-        <Image className='cardImage' src={image} alt={"Parent"} />
-        <h3>Name: {name}</h3>
+    <Container className ="card" align ="center">
+        <Image className='cardImage' src={image} alt={name} />
+        <h3>{name}</h3>
         <h3>Number of Pets: {numOfPets}</h3>
-        <h3>Years of Service: {serviceYears}</h3>
+        <h3>Years working with us: {serviceYears}</h3>
+        <button onClick ={handleClick}><Link to={`/parents/${id}/pets`}>Pets</Link></button>
     </Container>
   )
 }
