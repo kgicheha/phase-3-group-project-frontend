cbb977ba1ae6f168bf5c1031ecec6a8d31a7146c
Karter <105955246+kdliving14@users.noreply.github.com>
2022-08-25 21:25:16 -0700

    Update PetsCard.js
    updated for navigation

--- src/components/PetsCard.js
+++ src/components/PetsCard.js
@@ -1,12 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
-const PetsCard = ({image, name}) => {
+const PetsCard = ({image, name, setID, id}) => {
+  
+  const handleClick=()=>setID(id);
+ 
   return (
     <div>
         <img src={image} width={200} alt={name}/>
         <h3>{name}</h3>
+        <button onClick={handleClick}><Link to={`/pet/${id}`}>Sponsor me!</Link></button>
     </div>
   )
 }
+
 
 export default PetsCard
