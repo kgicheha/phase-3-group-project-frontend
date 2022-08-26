d85bf53ab58d43921c95ba6979321a1ef00554e8
Karter <105955246+kdliving14@users.noreply.github.com>
2022-08-25 21:10:14 -0700

    Update Nav Buttons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,21 @@
 import React from 'react'
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Button } from 'semantic-ui-react';
 
 
 function NavBar () {
   return (
-   <div>
+   <>
     <img id="logo" src={require("../Assets/logo.png")} alt="Logo" height="125"/>
-
+    <div className='navBar'>
     <Button.Group attached={true}>
-    <Button><NavLink to="/">Home</NavLink></Button>
-    <Button><NavLink to="/shelters">Shelters</NavLink></Button>
-    <Button><NavLink to="/parents">Parents</NavLink></Button>
-    <Button><NavLink to="/pets">Pets</NavLink></Button>
+    <Link to="/"><Button>Home</Button></Link>
+    <Link to="/shelters"><Button>Shelters</Button></Link>
+    <Link to="/parents"><Button>Parents</Button></Link>
+    <Link to="/pets"><Button>Pets</Button></Link>
     </Button.Group>
-    
-   </div>
+    </div>
+   </>
   )
 }
 
