e26f7413ccced1d54869dcc735fa417c01223de6
Karter <105955246+kdliving14@users.noreply.github.com>
2022-08-25 21:20:13 -0700

    Update ParentsAll.js
    updated for navigation

--- src/components/ParentsAll.js
+++ src/components/ParentsAll.js
@@ -3,34 +3,34 @@ import React, {useState, useEffect} from 'react'
 import ParentsCard from './ParentsCard';
 
 
-const ParentsAll = () => {
+const ParentsAll = ({setID, setS_P, iD}) => {
 
   const [parents, setParents] = useState([])
 
   useEffect(()=>{
-      fetch('http://localhost:9292/parents')
+      fetch(`http://localhost:9292/parents`)
       .then(res => res.json())
-      .then(data => {
-        setParents(data)
-      })
+      .then(data => {setParents(data)})
   },[]);
 
-  const parentCardData = parents.map((parent) => (
-    <ParentsCard
-      key = {parent.id}
-      parent_id = {parent.id}
-      image ={parent.image_url}
-      name = {parent.name}
-      numOfPets = {parent.current_num_pets}
-      serviceYears = {parent.service_years}
-    />
-  ))
-  
-    return (
-      <div>
-        {parentCardData}
-      </div>
-    )
+
+  return (
+    <div>
+      {parents.map((parent) => (
+        <ParentsCard
+          key = {parent.id}
+          id = {parent.id}
+          image ={parent.image_url}
+          name = {parent.name}
+          numOfPets = {parent.current_num_pets}
+          serviceYears = {parent.service_years}
+          setID={setID}
+          setS_P={setS_P}
+          iD = {iD}
+        />
+      ))}
+    </div>
+  )
   }
 
 export default ParentsAll
